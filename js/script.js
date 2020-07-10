a4e2b0e51e0193f9c30b51ffb7e236d2876cb60a
Filimon Mikhail <misafilimon255@mail.ru>
2020-07-10 19:25:29 +0300

    chore(js/script.js)corrected the code

--- js/script.js
+++ js/script.js
@@ -3,13 +3,7 @@
 
 const formatA = document.querySelector('.formatA'),
     formatB = document.querySelector('.formatB');
-const addNull = elem => {
-    if (elem.toString().length === 1) {
-        return elem = '0' + elem.toString().slice(0);
-    } else {
-        return elem;
-    }
-};
+const addNull = elem => (elem.toString().length === 1 ? elem = '0' + elem.toString().slice(0) : elem);
 const declOfNum = (number, titles) => {
     const cases = [2, 0, 1, 1, 1, 2];
     return titles[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5] ];
