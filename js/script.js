30580ee9dada210ddc5abbe6a35f50491ad121df
Filimon Mikhail <misafilimon255@mail.ru>
2020-07-14 12:34:14 +0300

    refactor(js/script.js)refactor code

--- js/script.js
+++ js/script.js
@@ -3,31 +3,34 @@
 
 const formatA = document.querySelector('.formatA'),
     formatB = document.querySelector('.formatB');
-const  ziroIfLessTen = elem => (elem < 10 ? elem = '0' + elem : elem);
+const  checkTheFirstNum = elem => (elem < 10 ? elem = '0' + elem : elem);
 const declOfNum = (number, titles) => {
     const cases = [2, 0, 1, 1, 1, 2];
     return titles[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5] ];
 };
 const ucFirst = str => str[0].toUpperCase() + str.slice(1);
-const ifAug = item => (item !== 'август' ? item.replace('ь', 'я') : item += 'a');
+const addEnding = item => (item !== 'август' ? item.replace('ь', 'я') : item += 'a');
 setInterval(() => {
-    const data = new Date(),
-        getYear = declOfNum(data.getFullYear(), ['год', 'года', 'года']),
-        getMonth = ifAug(data.toLocaleString('ru', { month: 'long' })),
-        getDay = ucFirst(data.toLocaleString('ru', { weekday: 'long' })),
-        getHours = declOfNum(data.getHours(), ['часов', 'час', 'часов']),
-        getMinut = declOfNum(data.getMinutes(), ['минуты', 'минута', 'минут']),
-        getSecond = declOfNum(data.getSeconds(), ['секунда', 'секунды', 'секунд']),
-        ziroIfLessTenInHours = ziroIfLessTen(data.getHours()),
-        ziroIfLessTenInMinute = ziroIfLessTen(data.getMinutes()),
-        ziroIfLessTenInSeconds = ziroIfLessTen(data.getSeconds());
+    const data = new Date();
 
-    const textYear = `${data.getFullYear()} ${getYear}`,
-        textMonth = `${data.getMonth()} ${getMonth}`,
-        textDay = `Сегодня ${getDay}`,
-        textHours =  `${ziroIfLessTenInHours} ${getHours}`,
-        textMinut =  `${ziroIfLessTenInMinute} ${getMinut}`,
-        textSecond =  `${ziroIfLessTenInSeconds} ${getSecond}`,
+    const year = data.getFullYear(),
+        hour = data.getHours(),
+        minut = data.getMinutes(),
+        second = data.getSeconds(),
+        arrYear = ['год', 'года', 'года'],
+        arrHours = ['часов', 'час', 'часов'],
+        arrMinuts = ['минуты', 'минута', 'минут'],
+        arrSeconds = ['секунда', 'секунды', 'секунд'];
+
+    const  month = addEnding(data.toLocaleString('ru', { month: 'long' })),
+        day = ucFirst(data.toLocaleString('ru', { weekday: 'long' }));
+
+    const textYear = `${data.getFullYear()} ${declOfNum(year, arrYear)}`,
+        textMonth = `${data.getMonth()} ${month}`,
+        textDay = `Сегодня ${day}`,
+        textHours =  `${checkTheFirstNum(hour)} ${declOfNum(hour, arrHours)}`,
+        textMinut =  `${checkTheFirstNum(minut)} ${declOfNum(minut, arrMinuts)}`,
+        textSecond =  `${checkTheFirstNum(second)} ${declOfNum(second, arrSeconds)}`,
         nowTime = data.toLocaleString('ru').replace(',', ' -');
 
     formatA.textContent = `${textDay}, ${textMonth} ${textYear}, ${textHours} ${textMinut} ${textSecond}`;
