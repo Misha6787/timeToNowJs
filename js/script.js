6c2625cf1de68c016f78b88a601793b581fe2f73
Filimon Mikhail <misafilimon255@mail.ru>
2020-07-15 13:06:50 +0300

    chore(js/script.js)corrected the function name

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 const formatA = document.querySelector('.formatA'),
     formatB = document.querySelector('.formatB');
-const  checkTheFirstNum = elem => (elem < 10 ? elem = '0' + elem : elem);
+const addZero = elem => (elem < 10 ? elem = '0' + elem : elem);
 const declOfNum = (number, titles) => {
     const cases = [2, 0, 1, 1, 1, 2];
     return titles[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5] ];
@@ -14,6 +14,8 @@ setInterval(() => {
     const data = new Date();
 
     const year = data.getFullYear(),
+        month = data.toLocaleString('ru', { month: 'long' }),
+        day = data.toLocaleString('ru', { weekday: 'long' }),
         hour = data.getHours(),
         minut = data.getMinutes(),
         second = data.getSeconds(),
@@ -22,15 +24,12 @@ setInterval(() => {
         arrMinuts = ['минуты', 'минута', 'минут'],
         arrSeconds = ['секунда', 'секунды', 'секунд'];
 
-    const  month = addEnding(data.toLocaleString('ru', { month: 'long' })),
-        day = ucFirst(data.toLocaleString('ru', { weekday: 'long' }));
-
     const textYear = `${data.getFullYear()} ${declOfNum(year, arrYear)}`,
-        textMonth = `${data.getMonth()} ${month}`,
-        textDay = `Сегодня ${day}`,
-        textHours =  `${checkTheFirstNum(hour)} ${declOfNum(hour, arrHours)}`,
-        textMinut =  `${checkTheFirstNum(minut)} ${declOfNum(minut, arrMinuts)}`,
-        textSecond =  `${checkTheFirstNum(second)} ${declOfNum(second, arrSeconds)}`,
+        textMonth = `${data.getMonth()} ${addEnding(month)}`,
+        textDay = `Сегодня ${ucFirst(day)}`,
+        textHours =  `${addZero(hour)} ${declOfNum(hour, arrHours)}`,
+        textMinut =  `${addZero(minut)} ${declOfNum(minut, arrMinuts)}`,
+        textSecond =  `${addZero(second)} ${declOfNum(second, arrSeconds)}`,
         nowTime = data.toLocaleString('ru').replace(',', ' -');
 
     formatA.textContent = `${textDay}, ${textMonth} ${textYear}, ${textHours} ${textMinut} ${textSecond}`;
